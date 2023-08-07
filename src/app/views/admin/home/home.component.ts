5a62e5bbc8b62b1be34cfe7287d8a44715d7d900
Emilian Ngatunga <emilianngatunga.dev@gmail.com>
2023-08-07 10:30:38 +0300

    Updates

--- src/app/views/admin/home/home.component.ts
+++ src/app/views/admin/home/home.component.ts
@@ -1,16 +1,29 @@
 import { Component, TemplateRef } from '@angular/core';
+import { NgbModal } from '@ng-bootstrap/ng-bootstrap';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
-  styleUrls: ['./home.component.scss']
+  styleUrls: ['./home.component.scss'],
 })
 export class HomeComponent {
+	closeResult = '';
 
 
-  constructor() {}
+  constructor(private modalService: NgbModal) {}
 
 
+
+	open(content: any) {
+		this.modalService.open(content, { ariaLabelledBy: 'modal-basic-title' }).result.then(
+			(result) => {
+				this.closeResult = `Closed with: ${result}`;
+			},
+			(reason) => {
+				this.closeResult = `Dismissed`;
+			},
+		);
+	}
 
 
 }
